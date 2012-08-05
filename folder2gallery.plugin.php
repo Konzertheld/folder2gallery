0c25bd6a7ed244afff2d0f72763ff10a6d745970
Konzertheld <christian.gredig@googlemail.com>
2012-08-05 21:05:24 +0200

    Initial commit

---
+++ folder2gallery.plugin.php
@@ -0,0 +1,63 @@
+<?php
+/*
+ * Folder2gallery Plugin
+ */
+
+class Folder2gallery extends Plugin
+{
+	/**
+	 * Run activation routines.
+	 */
+	public function action_plugin_activation()
+	{
+
+	}
+
+	/**
+	 * Run deactivation routines.
+	 */
+	public function action_plugin_deactivation( $file )
+	{
+
+	}
+
+	public function action_init()
+	{
+
+	}
+
+	public function filter_plugin_config( $actions, $plugin_id )
+	{
+		if ( $this->plugin_id() == $plugin_id ){
+			$actions[]= _t( 'Configure', 'folder2gallery' );
+		}
+		return $actions;
+	}
+
+	public function action_plugin_ui( $plugin_id, $action )
+	{
+		if ( $this->plugin_id() == $plugin_id ) {
+			switch ( $action ) {
+				case _t( 'Configure', 'folder2gallery' ):
+					$form = new FormUI( 'folder2gallery' );
+					
+					$form->append( 'submit', 'save', 'Save' );
+					$form->on_success( array($this, 'formui_submit' ) );
+					$form->out();
+					break;
+			}
+		}
+	}
+
+	public function formui_submit( FormUI $form )
+	{
+		Session::notice( _t( 'Folder2gallery options saved.', 'folder2gallery' ) );
+		$form->save();
+	}
+
+	public function action_form_publish( FormUI $form, Post $post)
+	{
+		
+	}
+}
+?>
